6dc882a0cb0a6e0f5052d5fcfe3aa3bce87c7b3f
sajina09 <sajinapathak0@gmail.com>
2023-10-07 21:34:04 +0545

    Add result modal

---
+++ src/components/ResultModal/index.tsx
@@ -0,0 +1,104 @@
+import { Button, Form, Modal } from "antd";
+import { FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./styles.css";
+
+export interface IProps {
+  visible: boolean;
+  hideModal: () => void;
+  navigateURL: string;
+  total: number;
+  score: number;
+  attempted: number;
+}
+
+const ResultModal: FC<IProps> = ({
+  visible,
+  hideModal,
+  navigateURL,
+  total,
+  score,
+  attempted,
+}) => {
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleFinish = ({ numberOfGroupA, numberOfGroupB }: any) => {
+    console.log("values", { numberOfGroupA, numberOfGroupB });
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    // navigate(
+    //   `${navigateURL}?a_count=${numberOfGroupA}&b_count=${numberOfGroupB}`
+    // );
+    console.log("navigateTo", navigateURL);
+    // hideModal();
+  };
+
+  const percentage = ((Number(score) / Number(total)) * 100).toFixed(2);
+  const passed = Number(percentage) >= 50;
+  return (
+    <>
+      <Modal
+        title="Customize your Questions"
+        onOk={form?.submit}
+        open={visible}
+        width={"50vw"}
+        onCancel={hideModal}
+        footer={
+          <div className="modal-footer">
+            <Button
+              key="back"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Back to Home
+            </Button>
+
+            <Button
+              key="submit"
+              type="primary"
+              loading={loading}
+              onClick={form?.submit}
+            >
+              Review your answers
+            </Button>
+          </div>
+        }
+      >
+        {/* TODO */}
+        <Form
+          form={form}
+          onFinish={handleFinish}
+          autoComplete="off"
+          colon={false}
+          initialValues={{
+            numberOfGroupA: "20",
+            numberOfGroupB: "5",
+          }}
+        >
+          <div className="result-container">
+            <p className="score-text">You have scored: {score}</p>
+            <p className="out-of-text">Out of: {total}</p>
+            <p className="out-of-text">Attempted: {attempted}</p>
+            <p className="percentage-text">Which is: {percentage}%</p>
+            <p className="pass-status-text">
+              You have {passed ? "passed" : "failed"}
+            </p>
+            <p className="emoji-text">
+              {passed
+                ? "😊 Keep moving forward."
+                : "😔 You need to work harder."}{" "}
+            </p>
+          </div>
+        </Form>
+      </Modal>
+    </>
+  );
+};
+
+export default ResultModal;
