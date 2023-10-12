8769f193c96e8989eb20d31b33c9ca14b3d6e293
sajina09 <sajinapathak0@gmail.com>
2023-10-12 09:03:23 +0545

    Add circualr text on page loader: striaght line for noe

--- src/hooks/usePageLoader.tsx
+++ src/hooks/usePageLoader.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "assets/logo.png";
+import "./CircleTextLoader.css"; // Import your CSS for the circular text animation
 
-const usePageLoader = (time = 1): [boolean, JSX.Element] => {
+const usePageLoader = (time = 1200): [boolean, JSX.Element] => {
   const [loader, setLoader] = useState<boolean>(true);
 
   useEffect(() => {
@@ -14,12 +15,19 @@ const usePageLoader = (time = 1): [boolean, JSX.Element] => {
 
   return [
     loader,
-    <div className={`fullscreen-loader `}>
-      <img
-        src={logo}
-        alt="Logo"
-        className={`logo-fullscreen ${loader ? "logo-fade-in" : ""}`}
-      />{" "}
+    <div className={`fullscreen-loader ${loader ? "loader-fade-out" : ""}`}>
+      <div className="circle-text-container">
+        <div className="circular-text">
+          <p>Become an Engineer - Be Er.</p>
+        </div>
+        <div className={"image-conatiner"}>
+          <img
+            src={logo}
+            alt="Logo"
+            className={`logo-fullscreen ${loader ? "logo-fade-in" : ""}`}
+          />
+        </div>
+      </div>
     </div>,
   ];
 };
